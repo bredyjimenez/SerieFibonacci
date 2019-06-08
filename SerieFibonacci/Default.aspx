71c6a4e893b4e5ff7674da3bd14f781ea7687536
adrian <badrianj@gmail.com>
2019-06-08 15:05:29 -0400

    Serie Fibonacci completa

---
+++ SerieFibonacci/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SerieFibonacci.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" AssociatedControlID="txtNumero" Text="Ingrese el número del que desea saber la serie fibonacci: "></asp:Label>
+            <asp:TextBox ID="txtNumero" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <br /><br /><br />
+            <asp:Button ID="btnCalcular" runat="server" Text="Calcular" OnClick="btnCalcular_Click" /><br /><br />
+            <asp:Label ID="lbFibonacci" runat="server" Text="Serie Fibonacci"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SerieFibonacci/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SerieFibonacci
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int primero = 0, segundo = 1, num = 0;
+
+            if (txtNumero.Text == "")
+            {
+                Response.Write("<script>alert('Debe ingresar una cifra numérica')</script>");
+            }
+            else
+            {
+                lbFibonacci.Text = primero + "," + segundo;
+
+                for (int i = 0; i < Convert.ToInt32(txtNumero.Text); i++)
+                {
+                    num = primero + segundo;
+                    if (num <= Convert.ToInt32(txtNumero.Text))
+                    {
+                        lbFibonacci.Text = lbFibonacci.Text + "," + num;
+                        primero = segundo;
+                        segundo = num;
+                    }
+                }
+            }
+        }
+    }
+}
